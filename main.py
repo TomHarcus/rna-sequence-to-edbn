3b033328b6999b349c7bee1cf6f8734ac0c91bc9
TomHarcus <tomharcus17@gmail.com>
2025-10-03 20:59:54 +0100

    link hotknot exec

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 from Bio import SeqIO
 import time
+import subprocess
 
 HOTKNOTS_EXEC = '/home/tom/Downloads/HotKnots_v2.0/bin/HotKnots'
 HOTKNOTS_DIRECTORY = '/home/tom/Downloads/HotKnots_v2.0/bin/'
@@ -10,14 +11,26 @@ OUTPUT = 'hotknots_output.csv'
 
 count = 0
 
-regex = "[BDEFHIJKLMNOPQRSTVWXYZ]"
+regex = "[^ACGU]"
 
 start = time.perf_counter()
 
 for record in SeqIO.parse(INPUT, 'fasta'):
+    current_strand = str(record.seq.upper()).replace('T', 'U')
+
     
-    if re.search(regex, str(record.seq.upper())):
-        count += 1
+    if not re.search(regex, current_strand) and len(current_strand) <= 20:
+        count+=1
+        
+   
+    """
+    command = [HOTKNOTS_EXEC, '-s', current_strand]
+    out = subprocess.run(command, cwd=HOTKNOTS_DIRECTORY, capture_output=True, text=True)
+    result = out.stdout.split('\n')
+    """
+    
+  
+    #print(out)
     
 
 print(count)
@@ -25,3 +38,6 @@ print(count)
 end = time.perf_counter()
 
 print(f"Time taken: {end-start}")
+
+
+
