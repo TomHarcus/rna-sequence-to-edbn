e55f55277ca73038db3541002c092cc018f7bd1b
TomHarcus <tomharcus17@gmail.com>
2025-10-03 14:10:16 +0100

    count how many examples

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("rna")
+from Bio import SeqIO
+
+HOTKNOTS_EXEC = "/home/tom/Downloads/HotKnots_v2.0/bin/HotKnots"
+HOTKNOTS_DIRECTORY = "/home/tom/Downloads/HotKnots_v2.0/bin/"
+
+INPUT = "Rfam.fa"
+OUTPUT = "hotknots_output.csv"
+
+count = 0
+
+for record in SeqIO.parse(INPUT, "fasta"):
+    count += 1
+
+print(count)
