5caf3d4ea355f6af2afd1feb5829736329db370c
TomHarcus <tomharcus17@gmail.com>
2025-10-03 14:36:06 +0100

    parsing rna sequences

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import re
 from Bio import SeqIO
+import time
 
-HOTKNOTS_EXEC = "/home/tom/Downloads/HotKnots_v2.0/bin/HotKnots"
-HOTKNOTS_DIRECTORY = "/home/tom/Downloads/HotKnots_v2.0/bin/"
+HOTKNOTS_EXEC = '/home/tom/Downloads/HotKnots_v2.0/bin/HotKnots'
+HOTKNOTS_DIRECTORY = '/home/tom/Downloads/HotKnots_v2.0/bin/'
 
-INPUT = "Rfam.fa"
-OUTPUT = "hotknots_output.csv"
+INPUT = 'Rfam.fa'
+OUTPUT = 'hotknots_output.csv'
 
 count = 0
 
-for record in SeqIO.parse(INPUT, "fasta"):
-    count += 1
+regex = "[BDEFHIJKLMNOPQRSTVWXYZ]"
+
+start = time.perf_counter()
+
+for record in SeqIO.parse(INPUT, 'fasta'):
+    
+    if re.search(regex, str(record.seq.upper())):
+        count += 1
+    
 
 print(count)
+
+end = time.perf_counter()
+
+print(f"Time taken: {end-start}")
